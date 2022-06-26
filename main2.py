1ca4967afb086ef42ff5265de80dca6ecab5dbe2
Myshiak.su <67818235+Myshiak@users.noreply.github.com>
2022-06-26 19:42:53 +0300

    Update main2.py
    Englified

--- main2.py
+++ main2.py
@@ -34,7 +34,7 @@ class Client_(Client):
     text_channel_list = []
     videos_list = []
     spamLimit = 4
-    commands = ['!бот', "!команды", "!лого"]
+    commands = ['!bot','!help','!logo']
     question_message_id = 813751244467470336
     roles = {
         'админ': 813123398656983090,
@@ -47,19 +47,19 @@ class Client_(Client):
         print('[INFO] Channel list is got')
         print(f'[INFO] logged in as @{self.user}')
     async def on_member_join(self, member):
-        await self.get_channel("793895190653173810").send(embed=Embed(title=f'{member} вступил на наш сервер',description=f'Добро пожаловать на наш сервер, {member}! Eleiśgenä Emes śeŕveŕte, {member}!',color=0xFFFF00))
+        await self.get_channel("793895190653173810").send(embed=Embed(title=f'{member} joined',description=f'Welcome, {member}! Leiszgenay szerveretume, {member}!',color=0xFFFF00))
     async def on_message(self, message: Message):
         author: Member = message.author
         content: str = message.content
         print(f'[MESSAGE] Content: {content}\nAuthor: {author}\n|')
         if author == Client().user:
             return
-        if content.lower() == "!бот":
+        if content.lower() == "!bot":
             await message.channel.send(
-                choice(['Да / Hes', "Ага / Ugju", "Чем могу помочь? / Kau kunem te luośo?", "Я тут / Hestem jeŕy"]))
-        elif content == '!команды':
-            await message.channel.send(embed=Embed(title='Мои команды:',description='\n!бот\n!команды-вывести команды\n!лимит спама [число сообщений]-установить лимит спама(для админов)\n!инфо-информация о сервере и боте\n!ава - вывести аватар\n!курс валюты [код валюты] - вывести курс валюты к рублю\n!видео [видео] - информация о видео с канала Myshiak.su\n\n',color=0xFFFF00))
-        elif content == '!инфо':
+                choice(['Да / Hes', "Ага / Agja", "Чем могу помочь? / Kei kanem lauszuent?", "Я тут / Hestem jeray"]))
+        elif content == '!help':
+            await message.channel.send(embed=Embed(title='Usage:',description='\n!bot\n!spam limit [number of messages]-set the spam limit(for staff)\n!info-information about the server and bot\n!avatar - avatar output\n!currency rate [currency code] - currency rate output\n!video [video\'s name] - information about a video from Myshiak.su\'s YouTube channel\n\n',color=0xFFFF00))
+        elif content == '!info':
             roles = []
             channels = []
             voice_channels = []
@@ -83,112 +83,112 @@ class Client_(Client):
             categories = ', '.join(categories)
             all_info = {
                 'ID': message.guild.id,
-                'Описание': str(message.guild.description),
-                'Регион': str(message.guild.region).capitalize(),
-                'Количество участников': message.guild.member_count,
-                'Роли': roles,
-                'Категории': categories,
-                'Текстовые каналы': channels,
-                'Голосовые каналы': voice_channels,
-                'Эмодзи': emojis
+                'Description': str(message.guild.description),
+                'Region': str(message.guild.region).capitalize(),
+                'Number of members': message.guild.member_count,
+                'Roles': roles,
+                'Categories': categories,
+                'Text channels': channels,
+                'Voice channels': voice_channels,
+                'Emojis': emojis
             }
-            info = Embed(title=f'Информация о сервере {message.guild.name}', description=f'', color=0xFFFF00)
+            info = Embed(title=f'Information about the server {message.guild.name}', description=f'', color=0xFFFF00)
             for i in all_info:
                 info.add_field(name=i, value=all_info[i], inline=False)
             info.set_image(url=message.guild.icon_url)
             await message.channel.send(embed=info)
-        elif content == '!ава':
-            e = Embed(title=f'Аватарка {message.author}', description='', color=0xFFFF00)
+        elif content == '!avatar':
+            e = Embed(title=f'Avatar of {message.author}', description='', color=0xFFFF00)
             e.set_image(url=message.author.avatar_url)
             await message.channel.send(embed=e)
-        elif match(r'!курс валюты [a-zA-Z]{3}', content.lower()):
+        elif match(r'!currency rate [a-zA-Z]{3}', content.lower()):
             currency = str(findall(r'[a-zA-Z]{3}', content)[0]).upper()
             try:
                 await message.channel.send(
-                    embed=Embed(title=f'Курс валюты {currency} к рублю:', description=str(valcursed(currency)) + ' ₽',
+                    embed=Embed(title=f'{currency} to ruble exchange rate:', description=str(valcursed(currency)) + ' ₽',
                                 color=0xFFFF00))
             except KeyError:
-                await message.channel.send(f"Валюты {currency} нет в списке")
-        elif match(r'!лимит спама \d+', str(content)):
+                await message.channel.send(f"The currency `{currency}` not found in the list")
+        elif match(r'!spam limit \d+', str(content)):
             roles = {}
             for role in message.author.roles:
                 roles[str(role)] = role
             if 'админ' in roles:
-                limit = int(str(content).replace('!лимит спама ', ''))
+                limit = int(str(content).replace('!spam limit ', ''))
                 if limit > 0:
-                    self.spamLimit = int(str(content).replace('!лимит спама ', ''))
-                    await message.channel.send(embed=Embed(title='Лимит спама',description=f'Лимит спама: {str(content).replace("!лимит спама ", "")} успешно установлен!',color=0xFFFF00))
+                    self.spamLimit = int(str(content).replace('!spam limit ', ''))
+                    await message.channel.send(embed=Embed(title='Spam limit',description=f'Spam limit: {str(content).replace("!spam limit ", "")} was successfully set!',color=0xFFFF00))
                 else:
-                    await message.channel.send('Лимит спама должен быть больше 0')
+                    await message.channel.send('Spam limit should be greater than 0')
             else:
-                await message.channel.send(f'{author.mention}, только админы могут использовать эту команду!')
-        elif match(r'!очистить \d+', str(content)):
+                await message.channel.send(f'{author.mention}, the command is only for staff!')
+        elif match(r'!purge \d+', str(content)):
             roles = {}
             for role in message.author.roles:
                 roles[str(role)] = role
             if 'админ' in roles:
-                limit = int(str(content).replace('!очистить ', ''))
+                limit = int(str(content).replace('!purge ', ''))
                 if limit > 0:
                     await message.channel.purge(limit=limit)
                     await message.channel.send(
-                        embed=Embed(title='Очистка', description=f'Очищено {limit} сообщений', color=0xFFFF00))
+                        embed=Embed(title='Purging', description=f'{limit} messages were deleted', color=0xFFFF00))
                 else:
-                    await message.channel.send('Аргумент должен быть больше 0')
+                    await message.channel.send('The argument should be greater than 0')
             else:
-                await message.channel.send(f'{author.mention}, только админы могут использовать эту команду!')
-        elif content == '!стоп':
+                await message.channel.send(f'{author.mention}, the command is only for staff!')
+        elif content == '!stop':
             roles = {}
             for role in message.author.roles:
                 roles[str(role)] = role
             if 'админ' in roles:
-                message.channel.send('Бот успешно выключен')
+                message.channel.send('Power off...')
                 quit()
             else:
-                await message.channel.send(f'{author.mention}, только админы могут использовать эту команду!')
-        elif match(r'!видео .+', content):
-            await message.channel.send('Пожалуйста, подождите...')
+                await message.channel.send(f'{author.mention}, the command is only for staff!')
+        elif match(r'!video .+', content):
+            await message.channel.send('Please wait...')
             cont = content[7:]
             self.videos_list = []
             for i in allVideos(channelId='UC2D-WeF4oMlyjlGq7hjRe9g', titles=True):
                 self.videos_list.append(i)
-            await message.channel.send('Список видео с канала получен')
+            await message.channel.send('The list of the channel\'s videos was got')
             has = False
             for i in self.videos_list:
                 if ratio(i[2].lower(), cont.lower()) > 30:
                     has = True
-                    statistic = f'Название: {i[2]}\nСсылка: {i[0]}\nID: {i[1]}\nДата: {i[3]}\nПросмотры: {i[4].replace("viewCount ", "")}\nЛайки: {i[5].replace("likeCount ", "")}\nДизлайки: {i[6].replace("dislikeCount ", "")}\nКомментарии: {i[8].replace("commentCount ", "")}'
+                    statistic = f'Name: {i[2]}\nLink: {i[0]}\nID: {i[1]}\nDate: {i[3]}\nViews: {i[4].replace("viewCount ", "")}\nLikes: {i[5].replace("likeCount ", "")}\nDislikes: {i[6].replace("dislikeCount ", "")}\nComments: {i[8].replace("commentCount ", "")}'
                     await message.channel.send(embed=Embed(title='Статистика', description=statistic, color=0xFFFF00))
                     break
             if not has:
-                await message.channel.send(f'Видео "{cont}" не найдено на канале')
-        elif match(r'!мут <#\d+>', content):
+                await message.channel.send(f'The video "{cont}" not found on the channel')
+        elif match(r'!mute <#\d+>', content):
             roles = {}
             for role in message.author.roles:
                 roles[str(role)] = role
             if 'админ' in roles:
                 chatId = findall('\d+', message.content)[0]
                 if chatId == '793895190653173810':
-                    await message.channel.send('Вы не можете замутить основной канал')
+                    await message.channel.send('You cannot mute the channel <#793895190653173810>')
                 else:
                     await message.channel.send(
-                        embed=Embed(title='Мут канала', description=f'Канал <#{chatId}> замьючен', color=0xFFFF00))
+                        embed=Embed(title='Mute channel', description=f'The channel <#{chatId}> was muted', color=0xFFFF00))
                     self.muted_channels_list.append(chatId)
             else:
-                await message.channel.send(f'{author.mention}, только админы могут использовать эту команду!')
-        elif match(r'!размут <#\d+>', content):
+                await message.channel.send(f'{author.mention}, the command is only for staff!')
+        elif match(r'!unmute <#\d+>', content):
             roles = {}
             for role in message.author.roles:
                 roles[str(role)] = role
             if 'админ' in roles:
                 chatId = findall('\d+', message.content)[0]
                 await message.channel.send(
-                    embed=Embed(title='Размут канала', description=f'Канал <#{chatId}> размьючен', color=0xFFFF00))
+                    embed=Embed(title='Unmute channel', description=f'The channel <#{chatId}> was unmuted', color=0xFFFF00))
                 if chatId in self.muted_channels_list:
                     self.muted_channels_list.remove(chatId)
             else:
-                await message.channel.send(f'{author.mention}, только админы могут использовать эту команду!')
-        elif content == '!gheuri' or content == '!гъюри':
-            await message.channel.send(choice(['Kau numenszt? Kau genen Se prgaletesz?', 'Szuni-Petergborgge hesterg un nebesti stute']))
+                await message.channel.send(f'{author.mention}, the command is only for staff!')
+        elif content == '!deirlenti':
+            await message.channel.send(choice(['Kei numenent? Kei hesten aktetima?', 'Szanktpeterburge hester an nebesti state']))
         else:
             self.spam.append([content, author])
             if isOneEl(self.spam, self.spamLimit):
@@ -196,11 +196,11 @@ class Client_(Client):
                 self.spam = []
             elif str(message.channel.id) in self.muted_channels_list:
                 await message.delete()
-            elif (str(message.content) == 'Пожалуйста, подождите...' or str(message.content) == 'Список видео с канала получен') and str(message.author) == 'Hydra.su#8971':
+            elif (str(message.content) == 'Please wait...' or str(message.content) == 'The list of the channel\'s videos was got') and str(message.author) == 'Myshiak.admin#8971':
                 await sleep(1)
                 await message.delete()
             else:
-                notAllowedPhrases = {'sex': ['секс', 'трах', 'презерватив', 'презик']}
+                notAllowedPhrases = {'sex': ['секс','трах','презерватив','презик','sex','penis','condom']}
                 if findall(r'\d', content) == ['8', '8', '0', '0', '5', '5', '5', '3', '5', '3', '5']:
                     await message.channel.send('Проще позвонить, чем у кого-то занимать')
                 elif 'это я' == content.lower():
